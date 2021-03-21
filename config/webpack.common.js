2ae37352d6bea230e50d9a635a844e6463756b43
jshuaishuai <ajiang115@qq.com>
2021-03-21 20:35:48 +0800

    feat:选择合适的分包

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,7 @@
 /*
  * @Descripttion:
  * @Author: Jason
- * @LastEditTime: 2021-03-20 15:49:25
+ * @LastEditTime: 2021-03-21 20:34:10
  */
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -171,7 +171,25 @@ module.exports = (options) => {
                 }),
             ],
             splitChunks: {
-                chunks: 'all',
+                chunks: 'all', // 默认作用于异步chunk
+                minSize: 0, // 默认值是30kb,代码块的最小尺寸
+                minChunks: 1, // 被多少模块共享,表示被引用次数，默认为1
+                maxAsyncRequests: 2, // 限制异步模块内部的并行最大请求数的，默认为5
+                maxInitialRequests: 4, // 限制入口的拆分数量 一个入口最大的并行请求数，默认为3
+                automaticNameDelimiter: '~', // 默认webpack将会使用入口名和代码块的名称生成命名,比如 'vendors~main.js'
+                cacheGroups: {
+                    vendors: {
+                        chunks: 'all',
+                        test: /node_modules/,
+                        priority: -10, /// 优先级，
+                    },
+                    commons: {
+                        chunks: 'all',
+                        minSize: 0, // 最小提取字节数
+                        minChunks: 2, // 最少被几个chunk引用
+                        priority: -20,
+                    },
+                },
             },
             runtimeChunk: {
                 name: (entrypoint) => `runtime-${entrypoint.name}`,
